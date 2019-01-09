d184eba1bf6edf1076d327d24b293bce4cb87182
xm <dxp157@sohu.com>
2019-01-09 21:52:13 +0800

    增加单文件组件 icon.vue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
 
 Vue.component('mw-button', Button);
+Vue.component('mw-icon', Icon);
 
 new Vue({
     el: '#app'
---
+++ src/icon.vue
@@ -0,0 +1,12 @@
+<template>
+   <svg class="icon" aria-hidden="true">
+       <use xlink:href="#icon-settings"></use>
+   </svg> 
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+
+</style>
+
