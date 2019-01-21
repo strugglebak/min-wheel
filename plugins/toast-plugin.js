f765c25a68c93d982af3f89b74def0bf8ab5c587
xm <dxp157@sohu.com>
2019-01-21 14:38:10 +0800

    修复测试中多次点击按钮出现多个 toast 组件的 bug

--- plugins/toast-plugin.js
+++ plugins/toast-plugin.js
@@ -1,17 +1,28 @@
 import Toast from '../src/toast'
 
+let currentToast;
+
 export default {
     install(Vue, options) {
         // add $toast method
         Vue.prototype.$toast = function (propsData) {
-            let Constructor = Vue.extend(Toast);
-            let toast = new Constructor({
-                propsData,
-            });
-            // the node in the toast component is text
-            toast.$slots.default = [propsData.text];
-            toast.$mount();
-            document.body.appendChild(toast.$el);
+            if(currentToast) {
+                currentToast.close();
+            }
+            currentToast = generateToastComponent(Vue, propsData);
         }
     }
+}
+
+function generateToastComponent(Vue, propsData) {
+    let Constructor = Vue.extend(Toast);
+    let toast = new Constructor({
+        propsData,
+    });
+    // the node in the toast component is text
+    toast.$slots.default = [propsData.text];
+    toast.$mount();
+    document.body.appendChild(toast.$el);
+
+    return toast;
 }
