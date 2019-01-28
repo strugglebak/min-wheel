c6d7b284b09ed42f0eb48ad6224d8a3647b63409
xm <dxp157@sohu.com>
2019-01-28 19:19:10 +0800

    使用 popover 与 document 监听 click 事件分开的方式实现 popover 的基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,17 +1,45 @@
 <template>
-    <div class="popover" v-on:click="trigger">
-        <div class="content-wrapper" v-if="isVisible">
+    <div class="popover" ref="popover">
+        <div class="content-wrapper" ref="contentWrapper"
+            v-if="isVisible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <div class="trigger-wrapper" ref="triggerWrapper">
+            <slot></slot>
+        </div>
     </div>   
 </template>
 <script>
 export default {
     name: 'MwPopover',
     methods: {
-        trigger() {
-            this.isVisible = !this.isVisible;
+        open() {
+            this.isVisible = true;
+            document.addEventListener('click', this.onDocumentClick);
+            console.log('document 监听了 click listener');
+        },
+        close() {
+            this.isVisible = false;
+            document.removeEventListener('click', this.onDocumentClick);
+            console.log('document 移除了 click listener')
+        },
+        onDocumentClick(e) {
+            console.log('document 被点击')
+            console.log(e.target)
+            if (!this.$refs.popover.contains(e.target)) {
+                this.close()
+            }
+        },
+        onPopoverClick(e) {
+            console.log('popover 被点击')
+            // if the scope of click inside the trigger-wrapper (include the trigger-wrapper)
+            if (this.$refs.triggerWrapper.contains(e.target)) {
+                if (!this.isVisible) {
+                    this.open();
+                } else {
+                    this.close();
+                }
+            }
         }
     },
     data() {
@@ -19,6 +47,14 @@ export default {
             isVisible: false,
         }
     },
+    mounted() {
+        this.$refs.popover.addEventListener('click', this.onPopoverClick);
+        console.log('popover 监听了 click')
+    },
+    destroyed() {
+        this.$refs.popover.removeEventListener('click', this.onPopoverClick);
+        console.log('popover 移除了 click')
+    }
 }
 </script>
 <style lang="scss" scoped>
@@ -31,6 +67,9 @@ export default {
             left: 0;
             bottom: 100%;
             border: 1px solid red;
+        }
+        > .trigger-wrapper {
+            padding: 1em;
         }
     }
 </style>
