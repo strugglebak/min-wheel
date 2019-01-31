d79799e715ee3da395234e09a3e7c0bb3250647f
xm <dxp157@sohu.com>
2019-01-31 15:06:40 +0800

    修改样式，增加 collapse 组件的下拉效果

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="collapse-item">
         <div class="collapse-item-title"
-            v-on:click="onTitleClick">
-            <mw-icon name="next"
-                v-bind:class="iconNameClasses">
-            </mw-icon>
+            v-on:click="onTitleClick"
+            v-bind:class="iconNameClasses"
+            >
+            <mw-icon name="next"></mw-icon>
             <span>{{title}}</span>
         </div>
-        <transition name="fade">
-            <div class="collapse-item-content"
-                v-if="isVisible">
-                <slot></slot>
+
+        <div class="grow" ref="grow">
+            <div class="collapse-item-content-wrapper" ref="contentWrapper">
+                <div class="collapse-item-content" ref="content"
+                    v-show="isVisible">
+                    <slot></slot>
+                </div>
             </div>
-        </transition>
+        </div>
+
     </div>   
 </template>
 <script>
 import Icon from './icon'
+import { inspect } from 'util';
 export default {
     name: 'MwCollapseItem',
     components: {
@@ -43,10 +48,15 @@ export default {
         open() {
             this.isVisible = true;
             this.iconDirection = 'down';
+            this.$nextTick(()=> {
+                let contentWrapper = this.$refs.contentWrapper;
+                this.$refs.grow.style.height = contentWrapper.getBoundingClientRect().height + "px";
+            });
         },
         close() {
             this.isVisible = false;
             this.iconDirection = 'right';
+            this.$refs.grow.style.height = 0;
         },
         onTitleClick() {
             if (!this.isVisible) {
@@ -75,47 +85,35 @@ export default {
 </script>
 <style lang="scss" scoped>
     .collapse-item {
-        .fade-enter-active, .fade-leave-active {
-            transition: opacity 0.3s;
-        }
-        .fade-enter, .fade-leave-to {
-            opacity: 0;
-        }
-
-        padding: 10px 0;
+        border: 1px solid rgba(153,153,153,1);
+        margin-top: -1px; margin-left: -1px; margin-right: -1px;
         > .collapse-item-title {
             cursor: pointer;
-            display: inline-flex;
-            align-items: center;
-            vertical-align: middle;
-            padding: 0 12px;
-            > .icon-right {
-                transform: rotate(0deg);
-                transition: all 0.3s;
+            display: flex; align-items: center; padding: 0 12px;
+            min-height: 32px;
+            &.icon-right { 
+                border-bottom: none;
+                > .mw-icon { transform: rotate(0deg); transition: all 0.3s; }
             }
-            > .icon-down {
-                transform: rotate(90deg);
-                transition: all 0.3s;
+            &.icon-down { 
+                border-bottom: 1px solid rgba(153,153,153,1);
+                > .mw-icon { transform: rotate(90deg); transition: all 0.3s; }
             }
         }
-        > .collapse-item-content {
-            border-top: 1px solid rgba(153,153,153,1);
-            margin-top: 10px;
-            padding-top: 10px;
-            padding-left: 12px;
-            padding-right: 12px;
-            will-change: height;
-        }
-        border: 1px solid rgba(153,153,153,1);
-        border-bottom: none;
         &:first-child {
-            border-top-left-radius: 4px;
-            border-top-right-radius: 4px;
+            border-top-left-radius: 4px; border-top-right-radius: 4px;
         }
         &:last-child {
-            border-bottom-left-radius: 4px;
-            border-bottom-right-radius: 4px;
-            border-bottom: 1px solid rgba(153,153,153,1);
+            border-bottom-left-radius: 4px; border-bottom-right-radius: 4px;
+        }
+        > .grow {
+            transition: height 0.3s;
+            overflow: hidden;
+            > .collapse-item-content-wrapper {
+                > .collapse-item-content {
+                    padding: 8px 12px;
+                }
+            }
         }
     }
 </style>
